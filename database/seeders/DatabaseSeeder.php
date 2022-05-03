9e3e23418d533a41a27898d3101104adb051372c
sm66707 <sm668830@gmail.com>
2022-05-02 22:28:34 -0400

    Add Admin seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use \App\models\CityManager;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,18 +14,18 @@ class DatabaseSeeder extends Seeder
      * @return void
      */
     public function run()
-    {
+    {   \App\Models\Admin::factory()->create();
         \App\Models\User::factory(10)->create();
-        \App\models\CityManager::factory(3)->create();
-        \App\models\Gym::factory(3)->create();
-        \App\models\CityManager::factory()->hasgyms(3)->create();
-        \App\models\GymManager::factory()->create();
-        \App\models\TrainingPackage::factory()->count(4)->create();
-        \App\models\Coach::factory(3)->create();
-        \App\models\TrainingSession::factory()->count(3)->create();
-        \App\models\coachTrainingSession::factory()->count(2)->create();
-        \App\models\TrainingPackageUser::factory()->count(2)->create();
-        \App\models\TrainingSessionUser::factory()->count(2)->create(['user_id'=>1, 'training_session_id'=>1]);
+        \App\Models\CityManager::factory(3)->create();
+        \App\Models\Gym::factory(3)->create();
+        \App\Models\CityManager::factory()->hasgyms(3)->create();
+        \App\Models\GymManager::factory()->create();
+        \App\Models\TrainingPackage::factory()->count(4)->create();
+        \App\Models\Coach::factory(3)->create();
+        \App\Models\TrainingSession::factory()->count(3)->create();
+        \App\Models\coachTrainingSession::factory()->count(2)->create();
+        \App\Models\TrainingPackageUser::factory()->count(2)->create();
+        \App\Models\TrainingSessionUser::factory()->count(2)->create(['user_id'=>1, 'training_session_id'=>1]);
 
     }
 }
