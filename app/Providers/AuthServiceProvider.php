1fb747345c61e54d53d88aecda1a58e31ff4c4c0
ranaehab56 <ranaehab56@gmail.com>
2022-05-07 15:21:27 +0200

    edit gym manger resource

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -26,13 +26,7 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
-/*
-        VerifyEmail::toMailUsing(function ($notifiable, $url) {
-            return (new MailMessage)
-                ->subject('Verify Email Address')
-                ->line('Click the button below to verify your email address.')
-                ->action('Verify Email Address', $url);
-        });*/
+
 
     }
 }
