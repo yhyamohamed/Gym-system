0adaf5fe6a206ed6542d941e1c84a497ff7ea02b
HassanTawfic <hatawfic@gmail.com>
2022-05-07 06:28:08 +0200

    fixing database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,11 +2,18 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Coach;
+use App\Models\coachTrainingSession;
+use App\Models\Gym;
+use App\Models\GymManager;
+use App\Models\TrainingPackage;
+use App\Models\TrainingPackageUser;
+use App\Models\TrainingSession;
+use App\Models\TrainingSessionUser;
 use Illuminate\Database\Seeder;
-use \App\models\CityManager;
+use \App\Models\CityManager;
 use App\Models\User;
-use App\Models\Position;
+
 
 
 class DatabaseSeeder extends Seeder
@@ -19,16 +26,16 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(PositionSeeder::class);
-        \App\Models\User::factory(20)->create();
-        \App\Models\CityManager::factory(3)->create();
-        \App\Models\Gym::factory(3)->create();
-        \App\Models\CityManager::factory()->hasgyms(3)->create();
-        \App\Models\GymManager::factory(User::where('possession_id',3)->count())->create();
-        \App\Models\TrainingPackage::factory()->count(4)->create();
-        \App\Models\Coach::factory(3)->create();
-        \App\Models\TrainingSession::factory()->count(3)->create();
-        \App\Models\coachTrainingSession::factory()->count(2)->create();
-        \App\Models\TrainingPackageUser::factory()->count(2)->create();
-        \App\Models\TrainingSessionUser::factory()->count(2)->create(['user_id' => 1, 'training_session_id' => 1]);
+        User::factory(20)->create();
+        CityManager::factory(3)->create();
+        Gym::factory(3)->create();
+        CityManager::factory()->hasgyms(3)->create();
+        GymManager::factory(User::where('position_id',3)->count())->create();
+        TrainingPackage::factory()->count(4)->create();
+        Coach::factory(3)->create();
+        TrainingSession::factory()->count(3)->create();
+        coachTrainingSession::factory()->count(2)->create();
+        TrainingPackageUser::factory()->count(2)->create();
+        TrainingSessionUser::factory()->count(2)->create(['user_id' => 1, 'training_session_id' => 1]);
     }
 }
